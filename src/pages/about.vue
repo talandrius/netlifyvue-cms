daa4e7272499fccc4cd977c1ed0d3f723eddb3dd
talandrius <talandrius@yahoo.fr>
2020-08-30 23:19:45 +0200

    Update about.vue

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,3 +3,13 @@
     <h1>About me !</h1>
   </div>
 </template>
+<script>
+export default {
+  metaInfo: {
+    title: 'About me ',
+    meta: [
+      { name: 'talandria', content: 'pourquoi le javascript' }
+    ]
+  }
+}
+</script>
