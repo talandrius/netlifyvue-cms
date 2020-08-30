886106baeba3dd01edbe201122aea5cfcacd4bc8
talandrius <talandrius@yahoo.fr>
2020-08-30 18:28:02 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: 'Apprendre le Javascript ',
+  siteDescription: 'Comment créer un site web facilement avec Gridsome Framework.',
 
   templates: {
     Post: '/:title',
